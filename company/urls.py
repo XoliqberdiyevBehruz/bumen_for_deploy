0f281dac3ad3769ad7808cd28db3b28133968e6c
Ulug'bek <119843506+ulugbekhusainovv@users.noreply.github.com>
2024-08-21 20:54:06 +0500

    feat(contact biz haqimizda)

--- company/urls.py
+++ company/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("contact/", ContactWithUsView.as_view(), name="contact_with_us"),
+    path("contact_with_us/", ContactWithUsView.as_view(), name="contact_with_us"),
     path("faqs/", FAQAPIView.as_view(), name="faqs"),
+    path('contact/', ContactsDetailView.as_view(), name="contact")
 ]
--- company/views.py
+++ company/views.py
@@ -3,10 +3,11 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
+from company.models import FAQ, ContactWithUs,Contacts
+from company.serializers import ContactWithUsSerializer, FAQSerializer,ContactsSerializer
 
-from company.models import FAQ, ContactWithUs
-from company.serializers import ContactWithUsSerializer, FAQSerializer
-
+# from .serializers import ContactsSerializer
 
 class ContactWithUsView(CreateAPIView):
     queryset = ContactWithUs.objects.all()
@@ -24,3 +25,14 @@ class FAQAPIView(APIView):
             return Response(serializer.data)
         except Exception:
             return Response(data={"message": "Internal Server Error"}, status=500)
+
+
+
+class ContactsDetailView(APIView):
+    def get(self, request):
+        try:
+            contact = Contacts.objects.first()
+            serializer = ContactsSerializer(contact)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Contacts.DoesNotExist:
+            return Response({"detail": "Contact not found."}, status=status.HTTP_404_NOT_FOUND)
